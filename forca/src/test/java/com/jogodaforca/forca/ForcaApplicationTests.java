76ba58aeba3b9572c52e0c911a64fd612cdd24cd
HugoNicolau <nicolau.hugogiles@gmail.com>
2025-04-13 17:18:53 -0300

    Add initial project structure and configuration files for Forca game

---
+++ forca/src/test/java/com/jogodaforca/forca/ForcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jogodaforca.forca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
